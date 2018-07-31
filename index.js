a4d7cfc60b2cf9120b4cb9f9a287e2b7e03489af
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-31 21:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+// // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var beatles = [];
+  for (let i = 0; i < musicians.length; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatles;
+}
+
+function johnLennonFacts(facts) {
+  
+  while (facts > 0) {
+   --facts;
+   
+   
+
+  }
+  return facts;
+}
